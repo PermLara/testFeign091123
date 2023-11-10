f88be32c191138d5f4a2833fa061662ed45cd1da
PermLara <lara_gil@mail.ru>
2023-11-11 01:43:27 +0300

    IndividualController has endpoints to get information in 4 formats, used mapstruct and feign

--- src/main/java/org/kata/ProfileFacadeApplication.java
+++ src/main/java/org/kata/ProfileFacadeApplication.java
@@ -2,14 +2,11 @@ package org.kata;
 
 import org.kata.feignclient.ProfileServiceFeignClient;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.cloud.openfeign.FeignAutoConfiguration;
 
 @EnableFeignClients(clients = ProfileServiceFeignClient.class)
 @SpringBootApplication
-@ImportAutoConfiguration(FeignAutoConfiguration.class)
 public class ProfileFacadeApplication {
 
     public static void main(String[] args) {
